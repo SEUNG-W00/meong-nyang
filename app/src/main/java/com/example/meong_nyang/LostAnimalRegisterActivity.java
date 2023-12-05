562802a06432ebf3695fc04cdc62754f87e5b056
kokodd1234@gmail.com <kokodd1234@gmail.com>
2023-12-05 17:38:11 +0900

    ..

--- app/src/main/java/com/example/meong_nyang/LostAnimalRegisterActivity.java
+++ app/src/main/java/com/example/meong_nyang/LostAnimalRegisterActivity.java
@@ -117,11 +117,20 @@ public class LostAnimalRegisterActivity extends AppCompatActivity {
                         Log.d("test", uri.toString());
                     } else {
                         // Handle the case where all ImageViews are occupied
-                        Toast.makeText(LostAnimalRegisterActivity.this, "You can only select up to 3 images", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LostAnimalRegisterActivity.this, "최대 3개의 이미지만 업로드 할 수 있습니다.", Toast.LENGTH_SHORT).show();
                     }
 
                 }
             });
+
+    private ImageView determineTargetImageView() {
+        for (ImageView imageView : imageViews) {
+            if (imageView.getDrawable() == null) {
+                return imageView;
+            }
+        }
+        return null;
+    }
 
     // CustomToolBar 반영하기
     @Override
@@ -139,8 +148,7 @@ public class LostAnimalRegisterActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.registercompletebtn) {
             // Handle the "Register" menu item click
             upload();
-        }
-        else if(item.getItemId() == android.R.id.home) {
+        } else if (item.getItemId() == android.R.id.home) {
             Intent intent = new Intent(getApplicationContext(), LostAnimalBoardActivity.class);
             startActivity(intent);
 
@@ -150,15 +158,7 @@ public class LostAnimalRegisterActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private ImageView determineTargetImageView() {
-        for (ImageView imageView : imageViews) {
-            if (imageView.getDrawable() == null) {
-                return imageView;
-            }
-        }
-        return null;
-    }
-
+    /*
     private void upload() {
         StorageReference storageReference = FirebaseStorage.getInstance().getReference("meong-nyang");
 
@@ -207,6 +207,96 @@ public class LostAnimalRegisterActivity extends AppCompatActivity {
         String tname= name.getText().toString();
         String tspecies= species.getText().toString();
         String tcallnum= callnum.getText().toString();
+
+        // Create a new LostAnimal object with relevant information
+        LostAnimal lostAnimal = new LostAnimal();
+
+        lostAnimal.setTitle(ttitle);
+        lostAnimal.setContent(tcontent);
+        lostAnimal.setLostlocation(tlostlocation);
+        lostAnimal.setLostdate(tlostdate);
+        lostAnimal.setLosttime(tlosttime);
+        lostAnimal.setSpecies(tspecies);
+        lostAnimal.setName(tname);
+        lostAnimal.setCallnum(tcallnum);
+        lostAnimal.setImage(imageUrl);
+        // Set other attributes of the LostAnimal object
+
+        // Push the LostAnimal object to Firebase Realtime Database
+        mDatabase.child("LostAnimal").push().setValue(lostAnimal).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LostAnimalRegisterActivity.this, "Data saved successfully", Toast.LENGTH_SHORT).show();
+                    // Clear or navigate to another screen after successful upload
+                } else {
+                    Toast.makeText(LostAnimalRegisterActivity.this, "Data save failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        // Push the LostAnimal object to Firebase Realtime Database under a single entry
+        mDatabase.child("LostAnimal").child(databaseEntryKey).setValue(lostAnimal)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // Clear or navigate to another screen after successful upload
+                            Toast.makeText(LostAnimalRegisterActivity.this, "Data saved successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LostAnimalRegisterActivity.this, "Data save failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+     */
+    private void upload() {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference("meong-nyang");
+
+        for (int i = 0; i < 3; i++) {
+            if (imageViews[i].getDrawable() != null && uris[i] != null) {
+                final int index = i;
+                storageReference.child("images/image" + (index + 1)).putFile(uris[i]).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            // Get the download URL of the uploaded image
+                            storageReference.child("images/image" + (index + 1)).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri downloadUri) {
+                                    // Save the image URL of the first image to Realtime Database
+                                    saveImageUrlToDatabase(downloadUri.toString());
+                                }
+                            });
+                            Toast.makeText(LostAnimalRegisterActivity.this, "Image" + (index + 1) + " upload success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LostAnimalRegisterActivity.this, "Image " + (index + 1) + " upload failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    private void saveImageUrlToDatabase(String imageUrl) {
+
+        // Define object
+        title = findViewById(R.id.title_et);
+        content = findViewById(R.id.content_et);
+        lostlocation = findViewById(R.id.lostlocation_et);
+        lostdate = findViewById(R.id.lostdate_et);
+        losttime = findViewById(R.id.losttime_et);
+        name = findViewById(R.id.name_et);
+        species = findViewById(R.id.species_et);
+        callnum = findViewById(R.id.callnum_et);
+
+        String ttitle = title.getText().toString();
+        String tcontent = content.getText().toString();
+        String tlostlocation = lostlocation.getText().toString();
+        String tlostdate = lostdate.getText().toString();
+        String tlosttime = losttime.getText().toString();
+        String tname = name.getText().toString();
+        String tspecies = species.getText().toString();
+        String tcallnum = callnum.getText().toString();
 
         // Create a new LostAnimal object with relevant information
         LostAnimal lostAnimal = new LostAnimal();
